1e6d10116f0e131f6107038c58294c9c1e6cd977
Xiang Li <sandylx@hotmail.com>
2018-11-24 15:55:28 +0800

    remove the lastest tag

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-export local_path=/Users/lixiang/Documents/docker/docker_jmeter/test/
-export container_path=/test/
+export local_path=/Users/lixiang/Documents/docker/docker_jmeter/test
+export container_path=/test
 
 rm ${local_path}/test.jtl 
 rm ${local_path}/test.log
 
 docker run \
   -v ${local_path}:${container_path} \
-  jmeter_xl:latest \
-  -n -t ${container_path}/test.jmx \
+  jmeter_xl -n \
+  -t ${container_path}/test.jmx \
   -l ${container_path}/test.jtl \
   -j ${container_path}/test.log \
   -Jv_data=${container_path}/data.csv
