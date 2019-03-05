f4e5a2b6827e700b6b7003b96aacd38d601ce7a1
Xiang Li <sandylx@hotmail.com>
2019-03-05 14:36:39 +0800

    change jmeter params

--- run_jmeter.sh
+++ run_jmeter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-export JVM_ARGS="-Xms1024m -Xmx1024m"
+export JVM_ARGS="-Xms2048m -Xmx2048m -Xss256k"
 echo "START Running jmeter on `date`"
-jmeter $@
+jmeter.sh $@
 echo "END Running jmeter on `date`"
